5117631548de3dc9fbdd688d3f3c5ffeab1a7015
yy1193889747 <1193889747@qq.com>
2019-06-25 20:54:24 +0800

    Default Changelist

--- src/main/java/com/cy/spiderman/repository/QuanMingRepository.java
+++ src/main/java/com/cy/spiderman/repository/QuanMingRepository.java
@@ -1,13 +1,14 @@
 package com.cy.spiderman.repository;
 
 import com.cy.spiderman.domain.ChangBa;
+import com.cy.spiderman.domain.QuanMing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author congyang.guo
  */
 
-public interface QuanMingRepository extends JpaRepository<ChangBa, Long> {
+public interface QuanMingRepository extends JpaRepository<QuanMing, String> {
 
 
 
